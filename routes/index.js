d84fce04a5276ca6680a5b397095b2e6789676e8
sgsshankar <sgsshankarnarayanan@gmail.com>
2016-02-14 23:17:55 +0530

    Scraper for profile and package. Added route for fetching details.

--- routes/index.js
+++ routes/index.js
@@ -18,6 +18,84 @@ router.get('/', function(req, res, next) {
 	});
 });
 
+/* GET details */
+router.get('/details/:username', function(req, res, next) {
+	var username = req.params.username;
+	var pdetails = {}
+	getProfileDetails(username).then(function(profile) {
+		pdetails.author = profile.author;
+		pdetails.authorImg = profile.authorImg;
+		pdetails.fullName = profile.fullName;
+		pdetails.links = profile.links;
+		pdetails.links.email = new Buffer((profile.links.email).replace(new RegExp("%", 'g'), ''), 'hex').toString("ascii");
+		packages = profile.content.packages;
+		pdetails.totalPackages = packages.length;
+		for (var i in packages) {
+			pkage = trim(packages[i]).split('\n')[0];
+			var pkage = getPackageDetails(pkage).then(function(pkagedetails) {
+				var plished = trim(pkagedetails.published).split('\n')[4];
+				var pd = {
+					packageName: pkagedetails.packageName,
+					published: plished,
+					stats: pkagedetails.stats
+				}
+			})
+		}
+	})
+	res.render('index', {
+		title: 'Profile details for npmjs.com'
+	});
+});
+
 module.exports = router;
 
 
+/* functions starts */
+function getProfileDetails(username) {
+	return new Promise(function(resolve, reject) {
+		url = base + '/~' + username;
+		x(url, {
+			author: '.content-column h1',
+			authorImg: '.sidebar .avatar img@src',
+			fullName: '.sidebar .fullname',
+			links: x('.sidebar .profile-sidebar-links', {
+				email: '.email a@data-email',
+				homepage: '.homepage a@href',
+				github: '.github a@href'
+			}),
+			content: x('.content-column', {
+				totalPackages: '#packages',
+				packages: x('.collaborated-packages', ['li'])
+			})
+		})(function(err, obj) {
+			if (err) {
+				reject(err)
+			} else {
+				profile = obj
+				resolve(profile)
+			}
+		})
+	});
+}
+
+function getPackageDetails(packagename) {
+	return new Promise(function(resolve, reject) {
+		url = base + '/package/' + packagename;
+		x(url, '.sidebar', {
+			published: '.last-publisher',
+			stats: {
+				daily: '.daily-downloads',
+				weekly: '.weekly-downloads',
+				monthly: '.monthly-downloads'
+			}
+		})(function(err, obj) {
+			if (err) {
+				reject(err)
+			} else {
+				packages = obj
+				packages.packageName = packagename
+				resolve(packages)
+			}
+		})
+	});
+}
