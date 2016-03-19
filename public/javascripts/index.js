81e1d7ef59c8ac0153f6c73acb79afc9ee18697e
sgsshankar <sgsshankarnarayanan@gmail.com>
2016-03-20 00:51:13 +0530

    UI for profile image and details

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,6 +4,15 @@ $(function() {
 			profile: $('#search').val()
 		};
 		$.get('/profile', parameters, function(data) {
+			console.log(data)
+			$('.main-content').addClass("show-things")
+			$('.profile-pic').attr("src",data.authorImg)
+			$('.fullname').html(data.fullName)
+			$('.email').html(data.links.email).attr("href","mailto:"+data.links.email)
+			$('.homepage').html(data.links.homepage).attr("href",data.links.homepage)
+			$('.github').html(data.links.github).attr("href",data.links.github)
+			$('.twitter').html(data.links.twitter).attr("href",data.links.twitter)
+			$('.freenode').html(data.links.freenode).attr("href",data.links.freenode)
 		});
 	});
 });
